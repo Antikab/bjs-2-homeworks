86d2c4377b875844d2509c66b63b72bd4ce57803
Arseny Prosvetov <arsprosvet@mail.ru>
2022-07-20 21:47:11 +0300

    1 и 2 дз - первый вариант

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,41 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+	let arr;
+	arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+
+	if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	}
+	return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+	let totalAmount;
 
-  // код для задачи №2 писать здесь
+	if (Number.isNaN(+percent)) {
+		return `Параметр \"Процентная ставка\" содержит неправильное значение \"${percent}\"`;
+	}
 
-  return totalAmount;
+	if (Number.isNaN(+contribution)) {
+		return `Параметр \"Первоначальный взнос\" содержит неправильное значение \"${contribution}\"`;
+	}
+
+	if (Number.isNaN(+amount)) {
+		return `Параметр \"Общая стоимость\" содержит неправильное значение \"${amount}\"`;
+	}
+
+	let credit = amount - contribution;
+	let today = new Date();
+	let rate = percent / 12 / 100;
+	let months = ((date.getMonth() - today.getMonth()) + ((date.getFullYear() - today.getFullYear()) * 12));
+	let monthPay = credit * (rate + (rate / (((1 + rate) ** months) - 1)));
+
+	totalAmount = +(monthPay * months).toFixed(2);
+
+	console.log(totalAmount);
+
+	return totalAmount;
 }
