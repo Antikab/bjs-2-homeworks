6a6443b04ede67d6c6474b3fcf39c34ca7bc6721
Arseny Prosvetov <arsprosvet@mail.ru>
2022-07-31 17:30:59 +0300

    первое решение

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
+	let result;
+	result = arr1.length === arr2.length & arr1.every((item, index) => item === arr2[index])
 
-  // Ваш код
-
-  return result; // boolean
+	return result; // boolean
 }
+
+compareArrays([1, 2, 3], [1, 2, 3]);
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+	let resultArr;
+	resultArr = arr.filter(num => num > 0).filter(num => num % 3 === 0).map(num => num * 10);
+	return resultArr; // array
 }
+
+advancedFilter([1, 2, -3, 4, 5, -6]);
+advancedFilter([1, 2, 3, 4, 5, 6]);
