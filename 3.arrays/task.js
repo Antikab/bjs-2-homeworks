ca230db8ccee55bc3da36a12a1d814b380223785
Arseny Prosvetov <arsprosvet@mail.ru>
2022-07-31 17:52:44 +0300

    подогнал под тест

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -6,6 +6,11 @@ function compareArrays(arr1, arr2) {
 }
 
 compareArrays([1, 2, 3], [1, 2, 3]);
+compareArrays([8, 9], [6]); // false, разные значения
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
+compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
+compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
+
 
 function advancedFilter(arr) {
 	let resultArr;
