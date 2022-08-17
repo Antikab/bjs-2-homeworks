dd4530cc3f742eb3ff344ae58c00b85b2a27db49
Arseny Prosvetov <arsprosvet@mail.ru>
2022-08-17 07:54:47 +0300

    задача

---
+++ 5.classes/task.js
@@ -0,0 +1,95 @@
+// Задача № 1
+
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this.state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		this.state = this.state * 1.5;
+	};
+
+	set state(state) {
+		if (state < 0) {
+			this._state = 0;
+		} else if (state > 100) {
+			this._state = 100;
+		} else {
+			this._state = state;
+		}
+	};
+
+	get state() {
+		return this._state;
+	};
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.author = author;
+		this.type = "book";
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "novel";
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "fantastic";
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(author, name, releaseDate, pagesCount);
+		this.type = "detective";
+	}
+}
+
+// Задача № 2
+
+class Library {
+	constructor(name) {
+		this.name = name;
+		this.books = [];
+	}
+
+	addBook(book) {
+		if (book.state > 30) {
+			this.books.push(book);
+		}
+	};
+
+	findBookBy(type, value) {
+		let search = this.books.find(book => book[type] === value);
+		return search ? search : null;
+	};
+
+	giveBookByName(bookName) {
+		let deleteBook = this.books.findIndex(book => book.name === bookName);
+
+		if (deleteBook === -1) {
+			return null;
+		} else {
+			return this.books.splice(deleteBook, 1)[0];
+		}
+	};
+}
